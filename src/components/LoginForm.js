3ab04495f1f7a2de065d19749a0af80512f06331
Lionel <lionel@pop-os.localdomain>
2020-08-11 11:36:43 +0200

    Login Page

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,6 +3,11 @@ import classes from "../assets/styles/components/LoginForm.module.css"
 import ButtonGroup from "react-bootstrap/ButtonGroup"
 import Button from "react-bootstrap/Button"
 import Form from "react-bootstrap/Form"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faEye } from "@fortawesome/free-solid-svg-icons"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+import { faFacebookF } from "@fortawesome/free-brands-svg-icons"
+
 function LoginForm() {
   return (
     <div className={classes.LoginForm}>
@@ -11,9 +16,14 @@ function LoginForm() {
         <p className={classes.LoginAlignTextLeft}>Login</p>
         <ButtonGroup aria-label="Basic example" className={classes.Space_btn}>
           <Button variant="secondary" className={classes.LoginWithGmail_btn}>
+            <FontAwesomeIcon icon={faEnvelope} className={classes.mailIcon} />
             Login with Gmail
           </Button>
           <Button variant="secondary" className={classes.LoginWithFacebook_btn}>
+            <FontAwesomeIcon
+              icon={faFacebookF}
+              className={classes.facebookIcon}
+            />
             Login with Facebook
           </Button>
         </ButtonGroup>
@@ -30,12 +40,13 @@ function LoginForm() {
             placeholder="Password"
             className={classes.input_Size}
           />
+          <FontAwesomeIcon icon={faEye} />
         </Form.Group>
         <p className={classes.alignTextRight}>Forgot Password?</p>
         <Button variant="primary" type="submit" className={classes.Login_btn}>
           <strong> Login</strong>
         </Button>
-        <p className={classes.alignTextRight}>Don't have an account?Sign Up</p>
+        <p className={classes.alignTextCenter}>Don't have an account?Sign Up</p>
       </Form>
     </div>
   )
