a96da1904f6ce824c5523624b25aa63132271c1a
Lionel <lionel@pop-os.localdomain>
2020-08-12 17:40:42 +0200

    created NavBar " Update Profile Page"

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,78 @@
+import React from "react"
+import Navbar from "react-bootstrap/Navbar"
+import ButtonGroup from "react-bootstrap/ButtonGroup"
+import Button from "react-bootstrap/Button"
+import Dropdown from "react-bootstrap/Dropdown"
+import classes from "../assets/styles/components/NavigationBar.module.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBell } from "@fortawesome/free-solid-svg-icons"
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons"
+import { faSortDown } from "@fortawesome/free-solid-svg-icons"
+
+function NavigationBar() {
+  return (
+    <div className={classes.navBarContainer}>
+      <Navbar bg="light" className={classes.Navbar}>
+        <ButtonGroup className={classes.ButtonGroup}>
+          <Button className={classes.Button}>Home</Button>
+          <Button className={classes.Button}>Loans</Button>
+          <Button className={classes.Button}>Reports</Button>
+          <FontAwesomeIcon
+            icon={faBell}
+            style={{
+              width: "130px",
+              color: "white",
+              height: "33px",
+              paddingTop: "12px",
+            }}
+          />
+          <p className={classes.UserName}>Marlene</p>
+          <FontAwesomeIcon
+            icon={faUserCircle}
+            style={{
+              width: "77px",
+              color: "white",
+              height: "40px",
+              marginTop: "10px",
+            }}
+          />
+          <FontAwesomeIcon
+            icon={faSortDown}
+            style={{
+              width: "27px",
+              color: "black",
+              height: "43px",
+              marginRight: "10px",
+              paddingRight: "4px",
+            }}
+          />
+
+          <Dropdown className={classes.Dropdown}>
+            <Dropdown.Toggle
+              variant="success"
+              id="dropdown-basic"
+              className={classes.DropdownToggle}
+            >
+              english
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Kinyarwanda</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">English</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          <FontAwesomeIcon
+            icon={faSortDown}
+            style={{
+              color: "#EFD917",
+              width: "27px",
+              height: "43px",
+            }}
+          />
+        </ButtonGroup>
+      </Navbar>
+    </div>
+  )
+}
+
+export default NavigationBar
--- src/views/UpdateProfile.js
+++ src/views/UpdateProfile.js
@@ -1,9 +1,11 @@
 import React from "react"
+import NavBar from "../components/NavigationBar"
+import classes from "../assets/styles/containers/UpdateProfile.module.css"
 
 function UpdateProfile() {
   return (
-    <div>
-      <h1>Update profile PAGE</h1>
+    <div className={classes.UpdateProfileContainer}>
+      <NavBar />
     </div>
   )
 }
