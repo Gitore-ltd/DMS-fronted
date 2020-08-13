33a1719a940706af3cf4997ab8b4dd6684f413f5
Niyonkuru@1 <kagororamaxime@gmail.com>
2020-08-13 03:18:41 -0700

    ft(landingPage): Landing Page
    [Finishes: #174297587]

--- src/components/DmsImage.js
+++ src/components/DmsImage.js
@@ -1,11 +1,11 @@
-import React from "react"
-import classes from "../assets/styles/components/DmsImage.module.css"
-import DMSImage from "../assets/images/DMS.png"
-function DmsImage() {
-  return (
-    <div className={classes.imageContainerSize}>
-      <img src={DMSImage} className={classes.imageSize} alt="DmsImage" />
-    </div>
-  )
-}
-export default DmsImage
+// import React from "react"
+// import classes from "../assets/styles/components/DmsImage.module.css"
+// import DMSImage from "../assets/images/DMS.png"
+// function DmsImage() {
+//   return (
+//     <div className={classes.imageContainerSize}>
+//       <img src={DMSImage} className={classes.imageSize} alt="DmsImage" />
+//     </div>
+//   )
+// }
+// export default DmsImage
