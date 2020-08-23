b74d14d06e9b0d7121f02497fc8310edff4a1c6d
Niyonkuru@1 <kagororamaxime@gmail.com>
2020-08-22 20:12:29 -0700

    ft(requests): view all my requests
    [Finsishes: #174459514]

--- src/store/constants.js
+++ src/store/constants.js
@@ -2,3 +2,5 @@ export const USER_SIGN_UP = 'USER_SIGN_UP';
 export const USER_SIGN_IN = 'USER_SIGN_IN';
 export const USER_PROFILE = 'USER_PROFILE';
 // export const GET_PROFILE = 'GET_PROFILE';
+
+export const MY_REQUESTS = 'MY_REQUESTS';
