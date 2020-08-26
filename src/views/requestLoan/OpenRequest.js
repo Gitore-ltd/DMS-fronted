235e8925a2d72018a58d09ff21b7449cbec10dd9
Lionel <lionel@pop-os.localdomain>
2020-08-26 17:37:40 +0200

    correct request loan page

--- src/views/requestLoan/OpenRequest.js
+++ src/views/requestLoan/OpenRequest.js
@@ -1,15 +1,14 @@
-import React, { useState } from 'react';
-import qs from 'qs';
-import './OpenRequest.css';
-import NavBar from '../../components/Navbar.js';
+import React, { useState } from "react"
+import qs from "qs"
+import "./OpenRequest.css"
+import NavBar from "../../components/Navbar.js"
 
 const OpenRequest = (props) => {
-  const queryParms = props.history.location.search;
+  const queryParms = props.history.location.search
 
   const [product, setProduct] = useState({
-    ...qs.parse(queryParms.replace('?', '')),
-  });
-
+    ...qs.parse(queryParms.replace("?", "")),
+  })
 
   return (
     <div className="MakeRequestcontainer">
@@ -25,24 +24,45 @@ const OpenRequest = (props) => {
             <form className="RequestLoanForm">
               <div className="OpenRequestFormGroup">
                 <label>Title</label>
-                <input type="text" value={product.title} />
+                <input
+                  type="text"
+                  value={product.title}
+                  style={{ marginLeft: "40px", width: "68%" }}
+                />
               </div>
               <div className="OpenRequestFormGroup">
                 <label>Available Quantity</label>
-                <input type="text" value={product.availableQuantity} />
+                <input
+                  type="text"
+                  value={product.availableQuantity}
+                  style={{ width: "70%" }}
+                />
               </div>
               <div className="OpenRequestFormGroup">
                 <label>Unit</label>
-                <input type="text" value={product.unit} />
+                <input
+                  type="text"
+                  value={product.unit}
+                  style={{ marginLeft: "40px", width: "68%" }}
+                />
               </div>
               <div className="OpenRequestFormGroup">
                 <label>Quality</label>
-                <input type="text" value={product.quality} />
+                <input
+                  type="text"
+                  value={product.quality}
+                  style={{ marginLeft: "32px", width: "68%" }}
+                />
               </div>
               <div className="OpenRequestFormGroup">
                 <label>Price</label>
-                <input type="text" value={product.price} />
+                <input
+                  type="text"
+                  value={product.price}
+                  style={{ marginLeft: "40px", width: "68%" }}
+                />
               </div>
+
               <div className="OpenRequestFormGroup">
                 <label>Description</label>
                 <textarea
@@ -51,15 +71,16 @@ const OpenRequest = (props) => {
                   rows="4"
                   cols="50"
                   value={product.description}
+                  className="textarea"
                 />
               </div>
-              <button>Request Loan</button>
+              <button className="btn_Request">Request Loan</button>
             </form>
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default OpenRequest;
+export default OpenRequest
